476ddf69d3157eb95f886b941024f7b7affc9893
amar251 <64704356+amar251@users.noreply.github.com>
2020-06-01 12:04:59 +0530

    final commit

--- app.js
+++ app.js
@@ -1,22 +1,60 @@
 (function(){
-  'use strict';
 
-  angular.module("LunchCheck",[])
+	angular.module('ShoppingListApp',[])
+	.controller('ToBuyController', ToBuy)
+	.controller('AlreadyBoughtController', Bought)
+	.service('ShoppingListCheckOffService',Service);
 
-  .controller("LunchCheckController", function($scope){
-    $scope.dishName="";
-    //$scope.message = "Please Enter Lunch Items";
-    $scope.dishCalculate = function(){
-      if($scope.dishName.length>0){
-        var a = $scope.dishName.split(",");
-        if(a.length>3){
-          $scope.message = "Too Much!";
-        }else if (a.length>0 && a.length<4) {
-          $scope.message = "Enjoy!"
-        }
-      }else{
-        $scope.message = "Please Enter Lunch Items";
-      }
-    };
-  });
+	ToBuy.$inject = ['ShoppingListCheckOffService'];
+	function ToBuy(ShoppingListCheckOffService){
+
+		var buy = this;
+		
+		buy.items=ShoppingListCheckOffService.ShowItems();
+		buy.BuyItem=function(itemIndex){
+			ShoppingListCheckOffService.removeItem(itemIndex);
+		};
+	}
+
+	Bought.$inject = ['ShoppingListCheckOffService'];
+	function Bought(ShoppingListCheckOffService){
+
+		var bought = this;
+		bought.items=ShoppingListCheckOffService.ShowBoughtItems();
+	}
+
+	function Service(){
+
+		var service=this;
+		var tobuy = [
+			{ name: "Cookies", quantity: "5"},
+			{ name: "Snacks", quantity: "5"},
+			{ name: "Soda", quantity: "6"},
+			{ name: "Sauce", quantity: "7"},
+			{ name: "Sugar", quantity: "1"}
+		];
+
+		var bought=[];
+
+		service.ShowItems=function(){
+			return tobuy;
+		};
+
+		service.ShowBoughtItems=function(){
+			return bought;
+		};
+
+		service.removeItem=function(itemIndex){
+			
+			bought.push(tobuy[itemIndex]);
+			tobuy.splice(itemIndex, 1);
+			
+		};
+
+		
+
+
+	}
+
+
 })();
